335107bcd99681f3e9ef5114f7e15bb7bc045f96
navyaagarwalsep-sudo <navyaagarwalsep@gmail.com>
2026-08-25 18:21:15 +0530

    area n perimeter using c++

---
+++ floatprog.cpp
@@ -0,0 +1,48 @@
+//float datatype
+#include <iostream>
+using namespace std;
+int main(){
+    system("cls");
+// float r,a ,c ;
+// const float PI=3.14;
+// cout<<"enter the radious";
+// cin>>r;
+// a=PI*r*r;  
+// c=2*PI*r;
+// cout<<"area of circle:   "<<a;
+// cout<<"\nthe circumference of circle is   " <<c;
+//    return 0;
+
+
+
+
+// int l,b,a,p;
+// cout<<"enter the length";
+// cin>>l;
+// cout<<"enter the breadth";
+// cin>>b;
+// a= l*b;
+// p=2*l+2*b;
+// cout<<"the area of the rectangle is:    "<<a;
+// cout<<"\nthe perimeter of the rectange is"<<p;
+// float a;
+// a=15/2.0;
+// cout<<a;
+// return 0;
+
+
+int h,e,m,s,c,total;
+float per;
+cout<<"enter the marks of all subject   ";
+cin>>h>> e>>m>>s>>c;
+total=h+e+m+s+c;
+cout<<"\ntotal is :  "<<total;
+per=total/5;
+cout<<"\npercentage is :    "<<per;
+return 0;
+
+
+
+
+
+}
